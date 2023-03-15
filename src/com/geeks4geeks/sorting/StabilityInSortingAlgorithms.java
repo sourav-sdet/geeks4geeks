7da125394c4b219af758ef8469d26982e824e0c7
Sourav <srvmishra.007@gmail.com>
2023-03-15 20:57:34 +0530

    Sorting Algo Overview and Bubble Sort

---
+++ src/com/geeks4geeks/sorting/StabilityInSortingAlgorithms.java
@@ -0,0 +1,26 @@
+package com.geeks4geeks.sorting;
+
+public class StabilityInSortingAlgorithms {
+
+	public static void main(String[] args) {
+
+
+		//arr[]={("Anil", 50), ("Ayan", 80), ("Piyush", 50), ("Ramesh", 80)}
+		
+		//Stability: If two items have same value, they should appear in the same order as in the original array
+		
+		//Sort arr in increasing order of marks
+		//arr[]={("Anil", 50), ("Piyush", 50), ("Ayan", 80), ("Ramesh", 80)} -- STABLE SORT
+		
+		
+		//Stability is required for non primitives (Array of objects)
+		//Stability is not required for primitives
+		
+		//Stable Sort Algorithms --> Bubble Sort, Insertion Sort, Merge Sort
+		//Unstable Sort Algorithms --> Selection Sort, Quick Sort, Heap Sort
+		
+		
+
+	}
+
+}
