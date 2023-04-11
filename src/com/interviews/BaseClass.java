ad097da2ed47c42a0283f5d4fc59dc9f40abfe20
Sourav <srvmishra.007@gmail.com>
2023-04-11 22:05:47 +0530

    Practice-3 April 11

---
+++ src/com/interviews/BaseClass.java
@@ -0,0 +1,17 @@
+package com.interviews;
+
+public class BaseClass {
+	
+	
+	public BaseClass(){
+		System.out.println("Base Class Constructor");
+	}
+
+
+	public void start() {
+		System.out.println("Base Class Method");
+		
+		
+	}
+
+}
